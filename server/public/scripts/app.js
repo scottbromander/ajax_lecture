3b8f05fda8cee7bb3532d6bfa95f854e4bdadae6
Scott Bromander <scott@primeacademy.io>
2015-11-16 13:55:05 -0600

    updated for jsonp call

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,13 +1,26 @@
-$(document).ready(function(){
+var apikey = "ca5d63fdb921e2570069eb8267389c4a9260227a";
+var baseUrl = "http://www.giantbomb.com/api";
+
+// construct the uri with our apikey
+var GamesSearchUrl = baseUrl + '/search/?api_key=' + apikey + '&format=jsonp';
+var query = "Batman";
+
+$(document).ready(function() {
+
+    // send off the query
     $.ajax({
-        type : "GET",
-        url  : "/kittyFooFoo",
-        success : function(data) {
-            appendDom(data);
-        }
+        url: GamesSearchUrl + '&query=' + encodeURI(query),
+        dataType: "jsonp",
+        crossDomain: true,
+        jsonp: 'json_callback',
+        success: searchCallback
     });
+
+
+    // callback for when we get back the results
+    function searchCallback(data) {
+        console.log(data);
+    }
+
+
 });
-
-function appendDom(data) {
-
-}
